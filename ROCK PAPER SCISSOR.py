310ff0efd1d0791651451c28dcaea7058c2cc819
Soumya Majumder <103351485+isoumya16@users.noreply.github.com>
2023-11-14 19:17:50 +0530

    Update ROCK PAPER SCISSOR.py

--- ROCK PAPER SCISSOR.py
+++ ROCK PAPER SCISSOR.py
@@ -1,26 +1,37 @@
 import random
 
+def get_user_choice():
+    user_choice = input("Enter your choice (Rock, Paper, or Scissors): ").capitalize()
+    while user_choice not in ["Rock", "Paper", "Scissors"]:
+        print("Invalid choice. Please enter Rock, Paper, or Scissors.")
+        user_choice = input("Enter your choice (Rock, Paper, or Scissors): ").capitalize()
+    return user_choice
+
+def get_computer_choice():
+    return random.choice(["Rock", "Paper", "Scissors"])
+
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         return "It's a tie!"
-    elif (user_choice == "Rock" and computer_choice == "Scissors") or \
-         (user_choice == "Scissors" and computer_choice == "Paper") or \
-         (user_choice == "Paper" and computer_choice == "Rock"):
+    elif (
+        (user_choice == "Rock" and computer_choice == "Scissors") or
+        (user_choice == "Scissors" and computer_choice == "Paper") or
+        (user_choice == "Paper" and computer_choice == "Rock")
+    ):
         return "You win!"
     else:
         return "Computer wins!"
 
-def main():
-    choices = ["Rock", "Paper", "Scissors"]
-    user_choice = input("Enter your choice (Rock, Paper, or Scissors): ").capitalize()
+def play_game():
+    user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
 
-    if user_choice in choices:
-        computer_choice = random.choice(choices)
-        print(f"Computer chooses: {computer_choice}")
-        result = determine_winner(user_choice, computer_choice)
-        print(result)
-    else:
-        print("Invalid choice. Please choose Rock, Paper, or Scissors.")
+    print(f"You choose {user_choice}.")
+    print(f"Computer choose {computer_choice}.")
 
-if __name__ == "__main__":
-    main()
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
+
+# Run the game
+play_game()
+
